658d47ab98dfbc017e6b098902f3deea07cf518b
Bhargav Rathod <bhargavrathod98@gmail.com>
2025-05-01 19:06:07 +0530

    email change

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -7,16 +7,20 @@ export async function sendVerificationEmail(email: string, name: string, token: 
   try {
     const verificationLink = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/verify-email?token=${token}`
     await resend.emails.send({
-      //from: 'no-reply@yourdomain.com',
-      from: 'onboarding@resend.dev',
+      // from: 'no-reply@yourdomain.com',
+      from: `${process.env.NEXT_PUBLIC_EMAIL_VERIFICATION_FROM_ADDRESS}`,
       to: email,
-      subject: 'Welcome to MyRoadmap.IO - Verify your email address',
+      subject: `Welcome to ${COMPANY_NAME} - Verify your email address`,
       html: `
         <div>
           <h1>Hi ${name},</h1>
           <p>Thank you for signing up at ${COMPANY_NAME}! Please verify your email address by clicking the link below:</p>
-          <p><a href="${verificationLink}">Verify Email</a></p>
+          <p><b><a href="${verificationLink}">Verify Email</a></b></p>
           <p>If you didn't create an account, you can safely ignore this email.</p>
+          <br/>
+          <br/>
+          <p>Thanks & Regards,</p>
+          <p>${COMPANY_NAME} Team</p>
         </div>
       `,
     })
